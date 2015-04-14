1c7b37898b650dc5f8d8ac16415f6c8ed0a80f25
Dave Petrillo <david.petrillo@gmail.com>
2015-04-14 00:58:51 -0400

    adding code

---
+++ ME160.ino
@@ -0,0 +1,106 @@
+/*************************************************** 
+  This is an example for the Adafruit Thermocouple Sensor w/MAX31855K
+
+  Designed specifically to work with the Adafruit Thermocouple Sensor
+  ----> https://www.adafruit.com/products/269
+
+  These displays use SPI to communicate, 3 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+// Default connection is using software SPI, but comment and uncomment one of
+// the two examples below to switch between software SPI and hardware SPI:
+
+// Example creating a thermocouple instance with software SPI on any three
+// digital IO pins.
+#define DO   3
+#define CS   4
+#define CLK  5
+#define SOL  6
+Adafruit_MAX31855 thermocouple(CLK, CS, DO);
+bool SOLstatus;
+
+// Example creating a thermocouple instance with hardware SPI (Uno/Mega only)
+// on a given CS pin.
+//#define CS   10
+//Adafruit_MAX31855 thermocouple(CS);
+
+//ADC Setup
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  
+  pinMode(SOL, OUTPUT);
+  digitalWrite(SOL, HIGH);
+  SOLstatus = true; 
+   
+  //Serial.println("Getting differential reading from AIN0 (P) and AIN1 (N)");
+  //Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+  
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  ads.setGain(GAIN_SIXTEEN);       // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  
+  ads.begin();
+  
+}
+
+void loop() {
+  // basic readout test, just print the current temp
+  // Serial.print("Internal Temp = ");
+  // Serial.println(thermocouple.readInternal());
+
+   double c = thermocouple.readCelsius();
+   if (isnan(c)) {
+    // Serial.println("Something wrong with thermocouple!");
+    Serial.print(0);
+   } else {
+    // Serial.print("C = "); 
+     Serial.print(c);
+   }
+   //Serial.print("F = ");
+   //Serial.println(thermocouple.readFarenheit());
+   
+   Serial.print(" ");
+   
+   int16_t results;
+   float multiplier = 0.0078125F; /* ADS1115  @ +/- 0.256V gain (16-bit results) */
+   results = ads.readADC_Differential_0_1();  
+    
+  //Serial.print("Differential: "); Serial.print(results); Serial.print("("); Serial.print(results * multiplier); Serial.println("mV)");
+   float scale =  2.F; /* PSI/mV*/
+   
+   Serial.print(results*multiplier*scale);
+   
+   Serial.print(" ");
+   
+   Serial.print(SOLstatus);
+   
+   Serial.print(" ");
+      
+   Serial.println(millis());
+   
+   
+}
---
+++ ReadData.py
@@ -0,0 +1,116 @@
+"""
+Display analog data from Arduino using Python (matplotlib)
+
+Adapted from code by:
+Author: Mahesh Venkitachalam
+Website: electronut.in
+
+Modified by Dave Petrillo for Cooper Union
+"""
+
+import sys, serial, argparse
+import numpy as np
+from time import sleep
+from collections import deque
+import csv
+import datetime
+
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+
+    
+# plot class
+class AnalogPlot:
+  # constr
+  def __init__(self, strPort, maxLen):
+      now = datetime.datetime.now()
+      datalog = open('Experiment Data '+ now.strftime("%Y-%m-%d %H:%M") +'.csv', 'wb')
+      dl = csv.writer(datalog)
+      dl.writerow(['Data Logged From Pressure Experiment'])
+      dl.writerow(['Temperature (C)', 'Pressure', 'Valve State', 'Time (mS)'])
+      # open serial port
+      self.ser = serial.Serial(strPort, 115200)
+
+      self.ax = deque([0.0]*maxLen)
+      self.ay = deque([0.0]*maxLen)
+      self.maxLen = maxLen
+      self.dl = dl.writerow
+
+  # add to buffer
+  def addToBuf(self, buf, val):
+      if len(buf) < self.maxLen:
+          buf.append(val)
+      else:
+          buf.pop()
+          buf.appendleft(val)
+
+  # add data
+  def add(self, data):
+      # assert(len(data) == 2)
+      self.addToBuf(self.ax, data[0])
+      self.addToBuf(self.ay, data[1])
+
+  # update plot
+  def update(self, frameNum, a0, a1):
+
+      try:
+          line = self.ser.readline()
+          data = [float(val) for val in line.split()]
+          self.dl(data)
+          # print data
+          #if(len(data) == 2):
+          self.add(data)
+          a0.set_data(range(self.maxLen), self.ax)
+          a1.set_data(range(self.maxLen), self.ay)
+      except KeyboardInterrupt:
+          print('exiting')
+      
+      return a0, 
+
+  # clean up
+  def close(self):
+      # close serial
+      self.ser.flush()
+      self.ser.close()    
+
+# main() function
+def main():
+  # create parser
+  parser = argparse.ArgumentParser(description="LDR serial")
+  # add expected arguments
+  parser.add_argument('--port', dest='port', required=True)
+
+  # parse args
+  args = parser.parse_args()
+  
+  #strPort = '/dev/tty.usbserial-A7006Yqh'
+  strPort = args.port
+
+  print('reading from serial port %s...' % strPort)
+
+  # plot parameters
+  analogPlot = AnalogPlot(strPort, 10000)
+
+  print('plotting data...')
+
+  # set up animation
+  fig = plt.figure()
+  ax = plt.axes(xlim=(0, 1000), ylim=(-200, 200))
+  a0, = ax.plot([], [])
+  a1, = ax.plot([], [])
+  anim = animation.FuncAnimation(fig, analogPlot.update, 
+                                 fargs=(a0, a1), 
+                                 interval=10)
+
+  # show plot
+  plt.show()
+  
+  # clean up
+  analogPlot.close()
+
+  print('exiting.')
+  
+
+# call main
+if __name__ == '__main__':
+  main()
